85e79b60657b7d9601b71548a936d9b0ca5b7344
Lucas <lemmg@cin.ufpe.br>
2018-11-29 16:14:12 -0200

    js depoimentos

---
+++ core/static/javascript/depoimentos.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.slider-active').slick({
+        prevArrow: '<button type="button" class="carousel-next" id="next"><img src="imagens/leftarrowangle1.svg" /></button>',
+        nextArrow: '<button type="button" class="carousel-prev"><img src="imagens/leftarrowangle1.svg" /></button>'
+
+    });
+  });
+
+  
+  
+  window.onload=function(){
+
+       avatar = new Array ("imagens/executivo.jpg","imagens/executivo2.jpg","imagens/executivo3.jpg");
+
+  document.getElementById("next").addEventListener("click", function(event){
+      document.getElementById("img").style.backgroundImage = avatar[1];
+  });
+  }      
+  

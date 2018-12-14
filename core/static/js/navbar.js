03d5774ddb160931745d7267bd3abf313cff25a3
Alecsandro <ascj@cin.ufpe.br>
2018-12-14 20:07:22 -0300

    Refatoração do js para resolver problemas de responsividade

--- core/static/js/navbar.js
+++ core/static/js/navbar.js
@@ -2,40 +2,7 @@ function myfunction(X) {
     X.classList.toggle("change");
 }
 
-$(document).ready(function () {
-    $('.container').click(function () {
-        $('ul').toggleClass('active');
-    })
-    $(window).resize(()=>{
-        
-       if($(window).width() >= 768){
-           $('ul').show();
-       }else{
-           $('ul').hide();
-       }
-    })
-})
-
-
-
 $('.container').click(function () {
-    $("#nav-mobile").slideToggle("slow", function () {
-        // Animation complete.
-    });
+    $("#nav-mobile").slideToggle("slow", function () {});
 });
 
-
-var $doc = $('html, body');
-$('.scroll').click(function() {
-$doc.animate({
-scrollTop: $( $.attr(this, 'href') ).offset().top
-}, 500);
-return false;
-});
-
-$('#logomarca').click(function() {
-$doc.animate({
-scrollTop: 0
-}, 500);
-return false;
-});

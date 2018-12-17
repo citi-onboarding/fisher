3bb265c1683f296906658cb8d4f8c5b4ad2ef685
Alecsandro <ascj@cin.ufpe.br>
2018-12-17 17:10:20 -0300

    Refatoração do mvv e sobre nos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import (
+    AboutUs,
+    MVV
+)
 # Register your models here.
+admin.site.register(AboutUs)
+admin.site.register(MVV)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class AboutUs(models.Model):
+    category = models.CharField('Categoria', max_length = 30, default='Sobre Nós')
+    image = models.ImageField('Imagem do Sobre nós', upload_to='aboutUs/', null=True)
+    description = models.TextField('Sobre nós', max_length=700)
+
+    def __str__(self):
+        return self.category
+    class Meta:
+        verbose_name = 'Sobre Nó' 
+
+
+class MVV(models.Model):
+    category = models.CharField('Categoria', max_length = 50, default = 'MVV')
+    mission = models.TextField('Missão', max_length = 500, default='')
+    vision = models.TextField('Visão', max_length=500, default='')
+    values = models.TextField('Valor', max_length=500, default='')
+
+    def __str__(self):
+        return self.category
+    class Meta:
+        verbose_name = 'MVV' 

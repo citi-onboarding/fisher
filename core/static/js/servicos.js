af1cfa5b0c55bce74047beffd81bb6d83687859f
Eder Marques <eder.marques@citi.org.br>
2019-01-14 13:14:20 -0300

    adding modal

--- core/static/js/servicos.js
+++ core/static/js/servicos.js
@@ -1,4 +1,5 @@
-$('.servicos-card').slick({
+$('document').ready(() => {
+  $('.servicos-card').slick({
     centerMode: true,
     dots: true,
     variableWidth: true,
@@ -6,3 +7,13 @@ $('.servicos-card').slick({
     prevArrow: '<button type="button"  class="service-next"><img src="/static/imagens/leftarrowangle1.svg"></button>',
     nextArrow: '<button type="button"  class="service-prev"><img src="/static/imagens/leftarrowangle1.svg"></button>',
   });
+
+  $('.solicitar-servico').click((e) => {
+    $('#servico-modal-container').css('display', 'flex');
+    $('#servico-select').val(e.target.getAttribute('data'));
+  });
+
+  $('#modal-close').click(() => {
+    $('#servico-modal-container').css('display', 'none');
+  });
+});

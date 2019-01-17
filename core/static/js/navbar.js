3e1dbc6cc5cd4b42662255070d605e3a2e951e64
Eder Marques <eder.marques@citi.org.br>
2019-01-17 11:14:05 -0300

    adjusts for deploy

--- core/static/js/navbar.js
+++ core/static/js/navbar.js
@@ -9,11 +9,17 @@ $('.container').click(function () {
 $('.navbar-link').on('click', function (event) {
   if (this.hash !== '') {
       event.preventDefault();
+      const adjusts = {
+        sobrenos: 20,
+        servicos: -30,
+        depoimentos: 0,
+        contato: 7,
+      };
       const hash = this.hash;
       const dist = ($(hash).offset().top - $(window).scrollTop()) / 1.5;
 
       $('html, body').animate({
-          scrollTop: $(hash).offset().top,
+          scrollTop: $(hash).offset().top + adjusts[hash.substr(1)],
       }, Math.abs(dist));
   }
 });
--- fisher/settings.py
+++ fisher/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+from decouple import config
+
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,17 +26,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!71vu$n$7f!at8i%+p%bxtmxfpj&m&7%gjzfdz2)^l!l*)7lxd'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'MY_EMAIL'
-EMAIL_HOST_PASSWORD = 'MY_PASSWORD'
+EMAIL_HOST_USER = config('MY_EMAIL')
+EMAIL_HOST_PASSWORD = config('MY_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
